50f8fe7c27b84c6589f7bf999a96fa6a05b9fb31
abhishekg785 <abhishekg785@gmail.com>
2016-01-03 12:27:49 +0530

    folders with username created using mkdirp

--- routes/uploadFile.js
+++ routes/uploadFile.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
+var mkdirp = require('mkdirp');
 
 //note !!! note !!! note !!!
 //so where this method came from
@@ -18,27 +19,44 @@ function isAuthenticate(req,res,next){
 
 var storage = multer.diskStorage({
   destination: function(req,file,cb) {
-    cb(null,'submissions')
+    var path = createDir(req); //function call to create a dir in submissions folder
+    console.log(path);
+    cb(null,path)
   },
   filename:function(req,file,cb){
     cb(null,file.fieldname + '-'+Date.now())
   }
 });
 
-router.get('/',function(req,res){
+router.get('/',isAuthenticate,function(req,res){
   res.render('uploadFile');
 });
 
+function createDir(req){
+  // console.log(req.user);
+  var username = req.user.username;
+  mkdirp('submissions/'+username,function(err){
+    if(!err){
+      console.log('dir created');
+    }
+    else{
+      console.log(err);
+    }
+  });
+  var path = 'submissions/'+username;
+  return path;
+}
+
 var upload = multer({storage : storage}).single('filename');
 
-router.post('/',function(req,res){
+router.post('/',isAuthenticate,function(req,res){
   upload(req,res,function(err){
     if(err){
       console.log(err);
       res.send('Error occured');
     }
     else{
-      console.log('done');
+      console.log('done uploading');
       res.send('File uploaded successfully');
     }
   });
